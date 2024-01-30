e642f3f6b11941a6e403c2179f9555e9c1cf1a43
Aryawastakn <aryas.bsc23@rvu.edu.in>
2024-01-30 10:24:15 +0530

    test

--- set01/problem09.c
+++ set01/problem09.c
@@ -3,7 +3,41 @@ float input();
 float square_root(float n);
 void output(float n, float sqrroot);
 
-int main()
+int main() 
 {
-    
+    float number, result;
+
+    number = input();
+    result = square_root(number);
+    output(number, result);
+
+    return 0;
+}
+
+float input() 
+{
+    float num;
+    printf("Enter a number: ");
+    scanf("%f", &num);
+    return num;
+}
+
+float square_root(float n) 
+{
+    float x = n;
+    float y = 1;
+    float epsilon = 0.0001; 
+
+    while (x - y > epsilon) 
+    {
+        x = (x + y) / 2;
+        y = n / x;
+    }
+
+    return x;
+}
+
+void output(float n, float sqrroot) 
+{
+    printf("Square root of %.2f is approximately %.4f\n", n, sqrroot);
 }
